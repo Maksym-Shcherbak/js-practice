49849b15fe94d5980035b1b00cda7eb4282619cf
Katysha94 <Vasilevakata76@gmail.com>
2023-07-01 14:49:18 +0300

    task_2_complete

--- js/script.js
+++ js/script.js
@@ -7,9 +7,30 @@
 //то показати через alert: "Вірно!"
 //в противному випадку відобразити:"Не знаєте? ECMAScript!"
 
-const answer = prompt("Яка офіційна назва JavaScript?");
-if (answer === "ECMAScript") {
-  alert("Вірно!");
-} else {
-  alert("Не знаєте? ECMAScript!");
-}
+// const answer = prompt("Яка офіційна назва JavaScript?");
+// if (answer === "ECMAScript") {
+//   alert("Вірно!");
+// } else {
+//   alert("Не знаєте? ECMAScript!");
+// }
+
+
+// TASK 2
+
+//Напишіть программу, яка отримує від користувача
+//число (кількість хвилин) и виводить у консоль
+//рядок у форматі годин та хвилин
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// 70 === 01:10
+
+
+const answer = prompt("Ввидіть число")
+let hours = Math.floor(answer / 60)
+hours = String(hours).padStart(2, "0")
+
+
+
+let minutes = answer % 60
+minutes = String(minutes).padStart(2, "0")
+console.log(`${hours}:${minutes}`)
+
